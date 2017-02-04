47b937aefd0dae375173bca2298189212b1de7a5
Ramya <nramya1996@gmail.com>
2017-02-05 01:02:54 +0530

    Java Assignment 2.2

---
+++ assignment2.2/src/assignment2_2.java
@@ -0,0 +1,10 @@
+
+public class assignment2_2 {
+	public static void main(String[] args) {
+		System.out.println("*_____*");
+		System.out.println("__*_*");
+		System.out.println("___*");
+		System.out.println("__*_*");
+		System.out.println("*_____*");
+	}
+}
